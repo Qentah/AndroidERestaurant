1ee9e7ac92b9c66939cdabe6803484971eae7f3a
Qentah(Quentin HENRY) <henryq.pro@outlook.fr>
2022-03-03 14:54:10 +0100

    new Layout

--- app/src/main/java/fr/isen/henry/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/henry/androiderestaurant/HomeActivity.kt
@@ -2,10 +2,31 @@ package fr.isen.henry.androiderestaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val button = findViewById<Button>(R.id.button)
+        val button2 = findViewById<Button>(R.id.button2)
+        val button3 = findViewById<Button>(R.id.button3)
+
+        button.setOnClickListener {
+            displayToast(button.text)
+        }
+        button2.setOnClickListener {
+            displayToast(button2.text)
+        }
+        button3.setOnClickListener {
+            displayToast(button3.text)
+        }
+
+    }
+
+    private fun displayToast(text: CharSequence){
+        Toast.makeText(this@HomeActivity, text, Toast.LENGTH_SHORT).show()
     }
 }
