3ed58fd120990fcdb6d2028fd6feef7c635c76a6
Qentah(Quentin HENRY) <henryq.pro@outlook.fr>
2022-03-04 16:35:37 +0100

    del unused

--- app/src/main/java/fr/isen/henry/androiderestaurant/CatSelectDetail.kt
+++ app/src/main/java/fr/isen/henry/androiderestaurant/CatSelectDetail.kt
@@ -82,18 +82,4 @@ class CatSelectDetail : AppCompatActivity() {
         }
     }
 
-    private fun generateLines() :ArrayList<Product>
-    {
-        val arrayList = ArrayList<Product>()
-        arrayList.add(Product(1,"Loup","Carnivore"))
-        arrayList.add(Product(2,"Gorille","Folivore et Frugivore"))
-        arrayList.add(Product(3,"Eléphant","Herbivore "))
-        arrayList.add(Product(4,"Tigre","Carnivore "))
-        arrayList.add(Product(5,"Dauphin","Carnivore "))
-        arrayList.add(Product(6,"Lion","Carnivore "))
-        arrayList.add(Product(7,"Panthère noire","Carnivore "))
-        arrayList.add(Product(8,"Requin Blanc","Carnivore "))
-        arrayList.add(Product(9,"Pygargue à tête blanche","Carnivore "))
-        return arrayList
-    }
 }
